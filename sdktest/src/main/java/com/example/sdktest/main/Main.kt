936588832a81351f13922889e603b0fe55b54500
Vini <vinicius.coelho@dock.tech>
2021-08-26 10:57:05 -0300

    First commit

---
+++ sdktest/src/main/java/com/example/sdktest/main/Main.kt
@@ -0,0 +1,11 @@
+package com.example.sdktest.main
+
+import com.example.sdktest.utilities.General
+
+class Main {
+
+    fun getText(): String {
+        return General().getText()
+    }
+
+}
---
+++ sdktest/src/main/java/com/example/sdktest/utilities/General.kt
@@ -0,0 +1,9 @@
+package com.example.sdktest.utilities
+
+class General {
+
+    internal fun getText(): String {
+        return "Text"
+    }
+
+}
