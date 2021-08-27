d88ace07cbb9c80d460b14fb002718347a4a405d
Vini <vinicius.coelho@dock.tech>
2021-08-27 11:45:25 -0300

    Workflow test / aar integrated

--- sdktest/src/main/java/com/example/sdktest/utilities/General.kt
+++ sdktest/src/main/java/com/example/sdktest/utilities/General.kt
@@ -1,9 +1,16 @@
 package com.example.sdktest.utilities
 
+import android.content.Context
+import com.scottyab.rootbeer.RootBeer
+
 class General {
 
     internal fun getText(): String {
         return "Text"
+    }
+
+    internal fun isRooted(context: Context): Boolean {
+        return RootBeer(context).isRooted
     }
 
 }
